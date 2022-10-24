4586385f6c6051c191bd2e1e6dc1263f5485c64d
Monir <monirhrabby.personal@gmail.com>
2022-10-24 18:46:35 +0600

    api created for get project data

--- controllers/v1/project.controller.js
+++ controllers/v1/project.controller.js
@@ -1,5 +1,17 @@
-const { createProject } = require("../../services/v1/project.services");
+const { createProject, getAllProjects } = require("../../services/v1/project.services");
 
+exports.getProjects = async (req, res, next) => {
+    try {
+        const result = await getAllProjects();
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: "can't get the data",
+            error: error.message,
+        });
+    }
+};
 exports.addProject = async (req, res, next) => {
     try {
         const result = await createProject(req.body);
--- services/v1/project.services.js
+++ services/v1/project.services.js
@@ -1,5 +1,9 @@
 const Project = require("../../models/v1/Project");
 
+exports.getAllProjects = async () => {
+    const project = await Project.find({});
+    return project;
+};
 exports.createProject = async data => {
     const project = await Project.create(data);
     return project;
